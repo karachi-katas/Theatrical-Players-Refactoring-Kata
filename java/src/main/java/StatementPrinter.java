c859ff175f79bcbd3eb6e1ca351d54af37b8dd25
Saad Ismail <saad.ismail@careem.com>
2020-06-12 23:27:36 +0500

    move from gradle to maven; java 10+ to 8

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -5,15 +5,15 @@ import java.util.Map;
 public class StatementPrinter {
 
     public String print(Invoice invoice, Map<String, Play> plays) {
-        var totalAmount = 0;
-        var volumeCredits = 0;
-        var result = String.format("Statement for %s\n", invoice.customer);
+        int totalAmount = 0;
+        int volumeCredits = 0;
+        StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
 
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (var perf : invoice.performances) {
-            var play = plays.get(perf.playID);
-            var thisAmount = 0;
+        for (Performance perf : invoice.performances) {
+            Play play = plays.get(perf.playID);
+            int thisAmount = 0;
 
             switch (play.type) {
                 case "tragedy":
@@ -39,12 +39,12 @@ public class StatementPrinter {
             if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            result.append(String.format("  %s: %s (%s seats)\n", play.name, numberFormat.format(thisAmount / 100), perf.audience));
             totalAmount += thisAmount;
         }
-        result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-        result += String.format("You earned %s credits\n", volumeCredits);
-        return result;
+        result.append(String.format("Amount owed is %s\n", numberFormat.format(totalAmount / 100)));
+        result.append(String.format("You earned %s credits\n", volumeCredits));
+        return result.toString();
     }
 
 }
--- java/src/test/java/StatementPrinterTests.java
+++ java/src/test/java/StatementPrinterTests.java
@@ -1,7 +1,8 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.approvaltests.Approvals.verify;
@@ -9,35 +10,39 @@ import static org.approvaltests.Approvals.verify;
 public class StatementPrinterTests {
 
     @Test
-    void exampleStatement() {
-        Map<String, Play> plays = Map.of(
-                "hamlet",  new Play("Hamlet", "tragedy"),
-                "as-like", new Play("As You Like It", "comedy"),
-                "othello", new Play("Othello", "tragedy"));
+    public void exampleStatement() {
+        Map<String, Play> plays = new HashMap<String, Play>() {{
+            put("hamlet", new Play("Hamlet", "tragedy"));
+            put("as-like", new Play("As You Like It", "comedy"));
+            put("othello", new Play("Othello", "tragedy"));
+        }};
 
-        Invoice invoice = new Invoice("BigCo", List.of(
+        Invoice invoice = new Invoice("BigCo", Arrays.asList(
                 new Performance("hamlet", 55),
                 new Performance("as-like", 35),
-                new Performance("othello", 40)));
+                new Performance("othello", 40)
+        ));
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        var result = statementPrinter.print(invoice, plays);
+        String result = statementPrinter.print(invoice, plays);
 
         verify(result);
     }
 
     @Test
-    void statementWithNewPlayTypes() {
-        Map<String, Play> plays = Map.of(
-                "henry-v",  new Play("Henry V", "history"),
-                "as-like", new Play("As You Like It", "pastoral"));
+    public void statementWithNewPlayTypes() {
 
-        Invoice invoice = new Invoice("BigCo", List.of(
+        Map<String, Play> plays = new HashMap<String, Play>() {{
+            put("henry-v", new Play("Henry V", "history"));
+            put("as-like", new Play("As You Like It", "pastoral"));
+        }};
+
+        Invoice invoice = new Invoice("BigCo", Arrays.asList(
                 new Performance("henry-v", 53),
                 new Performance("as-like", 55)));
 
         StatementPrinter statementPrinter = new StatementPrinter();
-        Assertions.assertThrows(Error.class, () -> {
+        Assert.assertThrows(Error.class, () -> {
             statementPrinter.print(invoice, plays);
         });
     }
