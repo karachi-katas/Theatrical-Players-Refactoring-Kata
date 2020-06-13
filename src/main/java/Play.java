ab03f334ffc6916973afc80557bbfa67700e2317
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-06-13 16:29:52 +0500

    moved calculate amount method to play

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,5 +1,16 @@
 public class Play {
 
+    public static final int TRAGEDY_BASE_AMOUNT = 40000;
+    public static final int TRAGEDY_AUDIENCE_THRESHOLD = 30;
+    public static final int TRAGEDY_PRICE_PER_ADDITIONAL_PERSON = 1000;
+    public static final int COMEDY_BASE_AMOUNT = 30000;
+    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
+    public static final int COMEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE = 10000;
+    public static final int COMEDY_PRICE_PER_ADDITIONAL_PERSON = 500;
+    public static final int COMEDY_PRICE_PER_PERSON = 300;
+    public static final int TRAGEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE = 0;
+    public static final int TRAGEDY_PRICE_PER_PERSON = 0;
+
     public String name;
     public String type;
 
@@ -7,4 +18,41 @@ public class Play {
         this.name = name;
         this.type = type;
     }
+
+    int getAmount(Performance perf) {
+        int thisAmount = 0;
+
+        switch (type) {
+            case "tragedy":
+                thisAmount = getTragedyAmount(perf.audience);
+                break;
+            case "comedy":
+                thisAmount = getComedyAmount(perf.audience);
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+        return thisAmount;
+    }
+
+    private int getComedyAmount(int audience) {
+        return getAmount(audience, COMEDY_BASE_AMOUNT, COMEDY_AUDIENCE_THRESHOLD, COMEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE, COMEDY_PRICE_PER_ADDITIONAL_PERSON, COMEDY_PRICE_PER_PERSON);
+    }
+
+    private int getAmount(int audience, int comedyBaseAmount, int comedyAudienceThreshold, int comedyFixedSurchargeForAdditionalAudience, int comedyPricePerAdditionalPerson, int comedyPricePerPerson) {
+        int thisAmount;
+        thisAmount = comedyBaseAmount;
+        if (audience > comedyAudienceThreshold) {
+            thisAmount += comedyFixedSurchargeForAdditionalAudience
+                    + comedyPricePerAdditionalPerson
+                    * (audience - comedyAudienceThreshold);
+        }
+        thisAmount += comedyPricePerPerson * audience;
+        return thisAmount;
+    }
+
+    private int getTragedyAmount(int audience) {
+        return getAmount(audience, TRAGEDY_BASE_AMOUNT, TRAGEDY_AUDIENCE_THRESHOLD, TRAGEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE, TRAGEDY_PRICE_PER_ADDITIONAL_PERSON, TRAGEDY_PRICE_PER_PERSON);
+    }
+
 }
--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -4,17 +4,6 @@ import java.util.Map;
 
 public class StatementPrinter {
 
-    public static final int TRAGEDY_BASE_AMOUNT = 40000;
-    public static final int TRAGEDY_AUDIENCE_THRESHOLD = 30;
-    public static final int TRAGEDY_PRICE_PER_ADDITIONAL_PERSON = 1000;
-    public static final int COMEDY_BASE_AMOUNT = 30000;
-    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
-    public static final int COMEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE = 10000;
-    public static final int COMEDY_PRICE_PER_ADDITIONAL_PERSON = 500;
-    public static final int COMEDY_PRICE_PER_PERSON = 300;
-    public static final int TRAGEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE = 0;
-    public static final int TRAGEDY_PRICE_PER_PERSON = 0;
-
     public String print(Invoice invoice, Map<String, Play> plays) {
         int totalAmount = 0;
         int volumeCredits = 0;
@@ -24,18 +13,7 @@ public class StatementPrinter {
 
         for (Performance perf : invoice.performances) {
             Play play = plays.get(perf.playID);
-            int thisAmount = 0;
-
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = getTragedyAmount(perf.audience);
-                    break;
-                case "comedy":
-                    thisAmount = getComedyAmount(perf.audience);
-                    break;
-                default:
-                    throw new Error("unknown type: ${play.type}");
-            }
+            int thisAmount = play.getAmount(perf);
 
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
@@ -51,24 +29,6 @@ public class StatementPrinter {
         return result.toString();
     }
 
-    private int getComedyAmount(int audience) {
-        return getAmount(audience, COMEDY_BASE_AMOUNT, COMEDY_AUDIENCE_THRESHOLD, COMEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE, COMEDY_PRICE_PER_ADDITIONAL_PERSON, COMEDY_PRICE_PER_PERSON);
-    }
 
-    private int getAmount(int audience, int comedyBaseAmount, int comedyAudienceThreshold, int comedyFixedSurchargeForAdditionalAudience, int comedyPricePerAdditionalPerson, int comedyPricePerPerson) {
-        int thisAmount;
-        thisAmount = comedyBaseAmount;
-        if (audience > comedyAudienceThreshold) {
-            thisAmount += comedyFixedSurchargeForAdditionalAudience
-                    + comedyPricePerAdditionalPerson
-                    * (audience - comedyAudienceThreshold);
-        }
-        thisAmount += comedyPricePerPerson * audience;
-        return thisAmount;
-    }
-
-    private int getTragedyAmount(int audience) {
-        return getAmount(audience, TRAGEDY_BASE_AMOUNT, TRAGEDY_AUDIENCE_THRESHOLD, TRAGEDY_FIXED_SURCHARGE_FOR_ADDITIONAL_AUDIENCE, TRAGEDY_PRICE_PER_ADDITIONAL_PERSON, TRAGEDY_PRICE_PER_PERSON);
-    }
 
 }
