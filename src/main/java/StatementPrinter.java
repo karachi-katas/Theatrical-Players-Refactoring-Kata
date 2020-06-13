e32879bc2ad436faba45f764f141e9cfa8a1e78c
Saif Ul Islam <saifulislam84210@gmail.com>
2020-06-13 17:16:03 +0500

    StatementPrinter - Added setter and constructor

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -1,33 +1,48 @@
+import javax.swing.plaf.nimbus.State;
 import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Map;
+        import java.util.Locale;
+        import java.util.Map;
 
 public class StatementPrinter {
 
-    public String print(Invoice invoice, Map<String, Play> plays) {
+    private StringBuilder result;
+    private int totalAmount;
+    private int totalVolumeCredits;
+    private NumberFormat numberFormat;
+
+    public StatementPrinter()
+    {
+        StringBuilder result;
         int totalAmount = 0;
-        int volumeCredits = 0;
+        int totalVolumeCredits = 0;
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);;
+    }
 
-        //
-        StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
-
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
-
-        for (Performance perf : invoice.performances) {
-            Play play = plays.get(perf.playID);
-            int thisAmount = play.getPlayAmount(perf.audience);
-
-            // add volume credits
-            volumeCredits += play.getVolumeCredits(perf.audience);
-
-            // print line for this order
-            result.append(String.format("  %s: %s (%s seats)\n", play.name,
-                    numberFormat.format(thisAmount / 100), perf.audience));
-            totalAmount += thisAmount;
-        }
+    public String print(Invoice invoice, Map<String, Play> plays) {
+        setStringBuilder(invoice.customer);
+        calculateTotal(invoice, plays, numberFormat);
         result.append(String.format("Amount owed is %s\n", numberFormat.format(totalAmount / 100)));
-        result.append(String.format("You earned %s credits\n", volumeCredits));
+        result.append(String.format("You earned %s credits\n", totalVolumeCredits));
         return result.toString();
     }
 
+    private void calculateTotal(Invoice invoice, Map<String, Play> plays, NumberFormat numberFormat) {
+        for (Performance perf : invoice.performances) {
+            Play play = plays.get(perf.playID);
+            int thisAmount = play.getPlayAmount(perf.audience);
+            totalVolumeCredits += play.getVolumeCredits(perf.audience);
+            appendResult(result, numberFormat, perf, play, thisAmount);
+            totalAmount += thisAmount;
+        }
+    }
+
+    private void appendResult(StringBuilder result, NumberFormat numberFormat, Performance perf, Play play, int thisAmount) {
+        result.append(String.format("  %s: %s (%s seats)\n", play.name,
+                numberFormat.format(thisAmount / 100), perf.audience));
+    }
+
+    public void setStringBuilder(String customer)
+    {
+        result = new StringBuilder(String.format("Statement for %s\n", customer));
+    }
 }
