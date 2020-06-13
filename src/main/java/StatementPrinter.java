330399122f8b1b54c17f0c7f2940d24d1b5e6ae1
Saad Ismail <saad.ismail@careem.com>
2020-06-13 17:28:03 +0500

    Extract out calculation from StatementPrinter

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -6,26 +6,38 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+    private Map<String, Play> plays;
+    StringBuilder result;
+    int totalAmount;
+    int volumeCredits;
+
+    NumberFormat numberFormat;
+
     public String print(Invoice invoice, Map<String, Play> plays) {
-        int totalAmount = 0;
-        int volumeCredits = 0;
-        StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
+        this.plays = plays;
+        this.totalAmount = 0;
+        this.volumeCredits = 0;
+        this.result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
+        this.numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        this.calculate(invoice);
 
-        for (Performance perf : invoice.performances) {
-            Play play = plays.get(perf.playID);
-
-            int thisAmount = play.calculateAmount(perf.audience);
-            // add volume credits
-            volumeCredits += play.calculateVolumeCredit(perf.audience);
-            // print line for this order
-            result.append(String.format("  %s: %s (%s seats)\n", play.name, numberFormat.format(thisAmount / 100), perf.audience));
-            totalAmount += thisAmount;
-        }
         result.append(String.format("Amount owed is %s\n", numberFormat.format(totalAmount / 100)));
         result.append(String.format("You earned %s credits\n", volumeCredits));
         return result.toString();
+    }
+
+    private void calculate(Invoice invoice) {
+        for (Performance performance : invoice.performances) {
+            Play play = plays.get(performance.playID);
+
+            int thisAmount = play.calculateAmount(performance.audience);
+            // add volume credits
+            this.volumeCredits += play.calculateVolumeCredit(performance.audience);
+            result.append(String.format("  %s: %s (%s seats)\n", play.name, numberFormat.format(thisAmount / 100), performance.audience));
+            this.totalAmount += thisAmount;
+        }
+
     }
 
 }
